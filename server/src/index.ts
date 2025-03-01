b6229780c124b17eaa8fd9ac4d5a7f1a195746f9
akshat <kshatsharmaaa@gmail.com>
2025-03-01 17:25:25 +0530

    checkout route

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import { userRouter } from "./routes/user";
+import { productRouter } from "./routes/product";
 import "dotenv/config";
 
 const app = express();
@@ -9,6 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/user", userRouter);
+app.use("/product", productRouter);
 
 const mongourl = process.env.MONGO_URL;
 if (!mongourl) {
